1a2b0efd8319d21c34e1c9b547a74881fb10d109
ASUS <ASUS@192.168.0.3>
2016-10-17 21:48:30 +0800

    新增Break.java以及Continue.java 修改StringComparator.java

---
+++ src/fcu/iecs/oop/Break.java
@@ -0,0 +1,19 @@
+package fcu.iecs.oop;
+import java.util.Scanner;
+public class  Break{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 0; i < 5; i++){
+			System.out.println(i);
+			if(i == 1)
+				break;
+		}
+		
+
+
+
+
+	}
+
+}
