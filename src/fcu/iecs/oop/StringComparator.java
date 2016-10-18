be3b31b87c18e94ae869084cafb70eb9a3b0a0e4
ASUS <ASUS@192.168.0.3>
2016-10-18 22:24:27 +0800

    修改為字串不同時重新執行

--- src/fcu/iecs/oop/StringComparator.java
+++ src/fcu/iecs/oop/StringComparator.java
@@ -1,4 +1,4 @@
-package ccc;
+package fcu.iecs.oop;
 
 import java.util.Scanner;
 
@@ -7,15 +7,37 @@ public class StringComparator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		  Scanner keyboard = new Scanner(System.in); 
-		//Scanner sc = new Scanner(System.in);
+		  String s1,s2;
 	    System.out.print("輸入第一字串: ");
-        String s1 = keyboard.next();
+        s1 = keyboard.next();
         System.out.print("輸入第二字串: ");
-        String s2 = keyboard.next();
+        s2 = keyboard.next();
         if(s1.equals(s2)) 
     		System.out.printf("The two strings are the same."); 
-    		else System.out.printf("The two strings are not the same."); 
+    		else 
+    		{
+    		    System.out.print("輸入第一字串: ");
+    	        s1 = keyboard.next();
+    	        System.out.print("輸入第二字串: ");
+    	        s2 = keyboard.next();
+    	        if(s1.equals(s2)) 
+        		System.out.printf("The two strings are the same."); 
+    	        else
+    	        {
+    	        	System.out.print("輸入第一字串: ");
+        	        s1 = keyboard.next();
+        	        System.out.print("輸入第二字串: ");
+        	        s2 = keyboard.next();
+        	        if(s1.equals(s2)) 
+                		System.out.printf("The two strings are the same."); 
+    	        }
+    		}
 
 	}
 
 }
+
+	      
+	        	
+	        	
+
